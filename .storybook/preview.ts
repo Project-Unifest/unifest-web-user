0bb8314a5fbdbb200fdc22d989cb3a754be2ac1c
unique0902 <rla0591@naver.com>
2024-06-20 17:04:08 +0900

    Feat: add storybook tailwindcss setting

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from '@storybook/react';
+import '../src/app/globals.css'; // replace with the name of your tailwind css file
 
 const preview: Preview = {
   parameters: {
