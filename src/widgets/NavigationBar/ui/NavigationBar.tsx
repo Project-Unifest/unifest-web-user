772e3519e11a18d87cdc13ec5f61cd99bf5e9858
unique0902 <rla0591@naver.com>
2024-07-26 14:05:14 +0900

    Feat: make navigation bar

--- src/widgets/NavigationBar/ui/NavigationBar.tsx
+++ src/widgets/NavigationBar/ui/NavigationBar.tsx
@@ -8,22 +8,36 @@ import {
   PaginationItem,
   PaginationLink,
 } from '@/shared/ui/Pagination/pagination';
+import HomeIcon from '@/shared/assets/icon/home_icon.svg';
+import MapIcon from '@/shared/assets/icon/map_icon.svg';
+import WaitingIcon from '@/shared/assets/icon/waiting_icon.svg';
+import MenuIcon from '@/shared/assets/icon/menu_icon.svg';
+
 const menuData = [
-  { id: 'menu01', name: '홈', path: '/home' },
-  { id: 'menu02', name: '지도', path: '/map' },
-  { id: 'menu03', name: '웨이팅', path: '/waiting' },
-  { id: 'menu04', name: '메뉴', path: '/menu' },
+  { id: 'menu01', name: '홈', path: '/home', icon: HomeIcon },
+  { id: 'menu02', name: '지도', path: '/map', icon: MapIcon },
+  { id: 'menu03', name: '웨이팅', path: '/waiting', icon: WaitingIcon },
+  { id: 'menu04', name: '메뉴', path: '/menu', icon: MenuIcon },
 ];
 const NavigationBar: React.FC = () => {
   const pathName = usePathname();
 
   return (
-    <Pagination>
-      <PaginationContent>
+    <Pagination className=' fixed bottom-0 left-0'>
+      <PaginationContent className='w-full'>
         {menuData.map((dt) => (
-          <PaginationItem>
-            <PaginationLink href={dt.path} isActive={pathName === dt.path}>
-              {dt.name}
+          <PaginationItem className='w-1/4 flex flex-row justify-center '>
+            <PaginationLink
+              href={dt.path}
+              isActive={pathName === dt.path}
+              className='w-full h-[56px]'
+            >
+              <div className='flex flex-col items-center gap-1'>
+                <dt.icon />
+                <p className=' text-[11px] text-[#525252] font-normal'>
+                  {dt.name}
+                </p>
+              </div>
             </PaginationLink>
           </PaginationItem>
         ))}
