c61e69cadb69e3c7547c1bc6de512ef7897e7454
unique0902 <rla0591@naver.com>
2024-07-28 01:24:55 +0900

    Feat: add calendar component

--- src/pages/Home/ui/Page.tsx
+++ src/pages/Home/ui/Page.tsx
@@ -1,8 +1,13 @@
+import { Calendar } from '@/shared/ui/Calendar/calendar';
 import PaginationExample from '@/shared/ui/Pagination/PaginationExample';
 import React from 'react';
 
 const HomePage: React.FC = () => {
-  return <div>asfas</div>;
+  return (
+    <div>
+      <Calendar />
+    </div>
+  );
 };
 
 export default HomePage;
