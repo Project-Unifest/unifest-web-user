9c9f067216080685467c36ec9eaddbab1f9d4326
unique0902 <rla0591@naver.com>
2024-08-09 20:59:20 +0900

    Style: add activated navigationBar style

--- src/widgets/NavigationBar/ui/NavigationBar.tsx
+++ src/widgets/NavigationBar/ui/NavigationBar.tsx
@@ -9,18 +9,51 @@ import {
   PaginationLink,
 } from '@/shared/ui/Pagination/pagination';
 import HomeIcon from '@/shared/assets/icon/home_icon.svg';
+import ActivatedHomeIcon from '@/shared/assets/icon/activated_home_icon.svg';
 import MapIcon from '@/shared/assets/icon/map_icon.svg';
+import ActivatedMapIcon from '@/shared/assets/icon/activated_map_icon.svg';
 import WaitingIcon from '@/shared/assets/icon/waiting_icon.svg';
+import ActivatedWaitingIcon from '@/shared/assets/icon/activated_waiting_icon.svg';
 import MenuIcon from '@/shared/assets/icon/menu_icon.svg';
+import ActivatedMenuIcon from '@/shared/assets/icon/activated_menu_icon.svg';
 import OnlyAppDialog from '@/widgets/OnlyAppDialog/ui/OnlyAppDialog';
-import { Button, buttonVariants } from '@/shared/ui/Button/button';
-import { cn } from '@/lib/utils';
+import { Button } from '@/shared/ui/Button/button';
 
-const menuData = [
-  { id: 'menu01', name: '홈', path: '/home', icon: HomeIcon },
-  { id: 'menu02', name: '지도', path: '/map', icon: MapIcon },
-  { id: 'menu03', name: '웨이팅', path: '/waiting', icon: WaitingIcon },
-  { id: 'menu04', name: '메뉴', path: '/menu', icon: MenuIcon },
+const menuData: {
+  id: string;
+  name: string;
+  path: string;
+  icon: any;
+  activatedIcon: any;
+}[] = [
+  {
+    id: 'menu01',
+    name: '홈',
+    path: '/home',
+    icon: HomeIcon,
+    activatedIcon: ActivatedHomeIcon,
+  },
+  {
+    id: 'menu02',
+    name: '지도',
+    path: '/map',
+    icon: MapIcon,
+    activatedIcon: ActivatedMapIcon,
+  },
+  {
+    id: 'menu03',
+    name: '웨이팅',
+    path: '/waiting',
+    icon: WaitingIcon,
+    activatedIcon: ActivatedWaitingIcon,
+  },
+  {
+    id: 'menu04',
+    name: '메뉴',
+    path: '/menu',
+    icon: MenuIcon,
+    activatedIcon: ActivatedMenuIcon,
+  },
 ];
 const NavigationBar: React.FC = () => {
   const pathName = usePathname();
@@ -51,8 +84,10 @@ const NavigationBar: React.FC = () => {
                 className='w-full h-[56px]'
               >
                 <div className='flex flex-col items-center gap-1'>
-                  <dt.icon />
-                  <p className=' text-[11px] text-[#525252] font-normal'>
+                  {pathName === dt.path ? <dt.activatedIcon /> : <dt.icon />}
+                  <p
+                    className={`text-[11px]  ${pathName === dt.path ? 'text-[#FF748A]' : 'text-[#525252]'} font-normal`}
+                  >
                     {dt.name}
                   </p>
                 </div>
