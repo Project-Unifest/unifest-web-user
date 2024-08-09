8cdc682cfeecc2ae117c7c082936ab435275edea
unique0902 <rla0591@naver.com>
2024-08-10 00:39:02 +0900

    Feat: custom calendar select day

--- src/pages/Home/ui/Page.tsx
+++ src/pages/Home/ui/Page.tsx
@@ -15,11 +15,24 @@ const HomePage: React.FC = () => {
     <div>
       <Accordion type='single' collapsible defaultValue='item-1'>
         <AccordionItem value='item-1'>
-          <AccordionOuter>
+          <AccordionOuter className='px-[19px] py-[27px]'>
             <Calendar showOnlyWeek={true} />
           </AccordionOuter>
-          <AccordionContent>
-            <Calendar showOnlyWeek={false} />
+          <AccordionContent className='px-[19px] py-[27px]'>
+            <div className='w-full relative'>
+              <div className='w-full flex flex-row items-center gap-[16px] absolute top-[40px] left-[10px]'>
+                <div className='before:w-[7px] before:h-[7px] before:rounded-full before:bg-[#1FC0BA] flex items-center gap-[6px] text-medium text-[12px] text-[#727276]'>
+                  1개
+                </div>
+                <div className='before:w-[7px] before:h-[7px] before:rounded-full before:bg-[#FF8A1F] flex items-center gap-[6px] text-medium text-[12px] text-[#727276]'>
+                  2개
+                </div>
+                <div className='before:w-[7px] before:h-[7px] before:rounded-full before:bg-[#FF5858] flex items-center gap-[6px] text-medium text-[12px] text-[#727276]'>
+                  3개 이상
+                </div>
+              </div>
+              <Calendar showOnlyWeek={false} />
+            </div>
           </AccordionContent>
           <AccordionTrigger></AccordionTrigger>
         </AccordionItem>
--- src/shared/ui/Calendar/calendar.tsx
+++ src/shared/ui/Calendar/calendar.tsx
@@ -2,13 +2,12 @@
 
 import * as React from 'react';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
-import { DayPicker } from 'react-day-picker';
+import { DayPicker, DayPickerProps } from 'react-day-picker';
 import { ko } from 'date-fns/locale';
 
 import { cn } from '@/lib/utils';
 import { buttonVariants } from '../Button/button';
 import { format } from 'date-fns';
-import { log } from 'console';
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 export type CustomProps = {
@@ -70,10 +69,47 @@ function Calendar({
     ? getRemainingDatesInMonthExcludingCurrentWeek()
     : [];
 
+  const daysWithOneFestival = [
+    new Date(2024, 7, 10),
+    new Date(2024, 7, 11),
+    new Date(2024, 7, 12),
+  ];
+  const daysWithTwoFestival = [
+    new Date(2024, 7, 13),
+    new Date(2024, 7, 14),
+    new Date(2024, 7, 15),
+  ];
+  const daysWithThreeFestival = [
+    new Date(2024, 7, 16),
+    new Date(2024, 7, 17),
+    new Date(2024, 7, 18),
+  ];
+  const [selectedDate, setSelectedDate] = React.useState<Date | undefined>();
+
+  const modifiers: DayPickerProps['modifiers'] = {};
+  if (selectedDate) {
+    modifiers.selected = selectedDate;
+  }
+
   return (
     <DayPicker
       showOutsideDays={showOutsideDays}
-      className={cn('p-3', className)}
+      modifiers={{
+        one: daysWithOneFestival,
+        two: daysWithTwoFestival,
+        three: daysWithThreeFestival,
+        ...modifiers,
+      }}
+      onDayClick={(day, modifiers) => {
+        setSelectedDate(day);
+      }}
+      modifiersClassNames={{
+        one: 'after:w-[7px] after:h-[7px] after:rounded-full after:bg-[#1FC0BA] after:absolute after:bottom-0 after:left-[calc(50%-3.5px)]',
+        two: 'after:w-[7px] after:h-[7px] after:rounded-full after:bg-[#FF8A1F] after:absolute after:bottom-0 after:left-[calc(50%-3.5px)]',
+        three:
+          'after:w-[7px] after:h-[7px] after:rounded-full after:bg-[#FF5858] after:absolute after:bottom-0 after:left-[calc(50%-3.5px)]',
+      }}
+      className={cn('', className)}
       locale={ko}
       disabled={remainingDates}
       formatters={{
@@ -83,7 +119,7 @@ function Calendar({
         months:
           'flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0 w-full',
         month: 'space-y-4 w-full',
-        caption: `${showOnlyWeek ? 'hidden' : 'flex'} pl-2 pt-1 relative items-center`,
+        caption: `${showOnlyWeek ? 'hidden' : 'flex mb-[52px]'} pl-2 pt-1 relative items-center`,
         caption_label: ' text-[24px] font-bold',
         nav: 'space-x-1 flex items-center',
         nav_button: cn(
@@ -96,15 +132,13 @@ function Calendar({
         head_cell:
           'text-muted-foreground rounded-md flex-1 font-normal text-[0.8rem]',
         row: 'flex w-full mt-0 h-auto',
-        cell: 'h-auto flex-1 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20',
-        day: cn(
-          buttonVariants({ variant: 'ghost' }),
-          'h-9 w-16 p-0 font-normal aria-selected:opacity-100'
-        ),
+        cell: 'h-auto flex-1 text-center flex items-center justify-center hover:bg-accent text-sm p-0 relative [&:has([aria-selected])]:hover:cursor-pointer ',
+        day: 'h-[48px] w-full p-0 font-normal aria-selected:opacity-100',
         day_range_end: 'day-range-end',
         day_selected:
-          'bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground',
-        day_today: 'bg-accent text-accent-foreground',
+          'text-white w-[20px] h-[20px] rounded-[100%] bg-[#FF748A] flex items-center justify-center text-center',
+        day_today:
+          'text-[#FF748A] w-[20px] h-[20px] rounded-[100%] bg-white flex items-center justify-center text-center border border-[#FF748A]',
         day_outside:
           'day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30',
         day_disabled: 'hidden',
